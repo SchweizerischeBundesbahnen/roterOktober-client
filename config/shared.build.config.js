3c0dc5e80779a8e59308500e06c6d290c2d4728e
Stefan Urech <stefan@surech.ch>
2016-07-19 21:13:57 +0200

    stu: Grundstruktur mit ESTA WebJS angelegt

---
+++ config/shared.build.config.js
@@ -0,0 +1,26 @@
+/*
+ * @author u215942 (Stefan Zeller)
+ * @version: 1.0.1
+ * @since 04.04.2016
+ */
+
+exports.webpackLoaders = [
+    {
+        test: /\.js$/, exclude: [/node_modules/], loader: 'babel'
+    }, {
+        test: /\.json$/, loader: 'json'
+    }, {
+        test: /\.html$/, loader: 'raw'
+    }, {
+        test: /\.css$/, loader: 'style!css'
+    }, {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader'
+    }, {
+        test: /\.(woff|woff2)$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+    }, {
+        test: /\.ttf$/, loader: 'file-loader'
+    }, {
+        test: /\.eot$/, loader: 'file-loader'
+    }
+];
