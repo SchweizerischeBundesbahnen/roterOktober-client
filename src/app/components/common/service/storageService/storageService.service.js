8d132e6ffd208280744dbc3ccb6b03f256366d03
Stefan Urech <stefan@surech.ch>
2016-10-29 12:30:17 +0200

    stu: Ein fehlendes ngInject führte zu einem Fehler nach der Minifizierung. Hiermit ist dies behoben.

--- src/app/components/common/service/storageService/storageService.service.js
+++ src/app/components/common/service/storageService/storageService.service.js
@@ -2,6 +2,7 @@ const SEARCH_KEY = 'searchTerm';
 
 export default class StorageService{
 
+  /*@ngInject*/
   constructor(localStorageService){
     this.localStorageService = localStorageService;
     this.searchTerm = localStorageService.get(SEARCH_KEY) ? localStorageService.get(SEARCH_KEY) : '';
